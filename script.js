cc47e014f95bd74868eda1724dcfc207e641682b
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-01-23 10:46:45 +0200

    new files add

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-const darkBtn = document.querySelector(".dark");
-const lightBtn = document.querySelector(".light");
+ document.addEventListener("DOMContentLoaded", function(){
+    const darkBtn = document.querySelector(".dark");
+    const lightBtn = document.querySelector(".light");
+    const display = document.querySelector("#display");
+    const buttons = document.querySelectorAll("#buttons button");
 
-darkBtn.addEventListener("click" , () => {
-    document.documentElement.classList.remove("light");
-    document.documentElement.classList.add("dark");
-});
 
-lightBtn.addEventListener("click" , () =>{
-    document.documentElement.classList.remove("dark");
-    document.documentElement.classList.add("light");
-})
+    // darkmode theme activated
+    darkBtn.addEventListener("click" , () => {
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.add("dark");
+    });
+    
+    //lightmode theme activated
+    lightBtn.addEventListener("click" , () =>{
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.add("light");
+    });
+
+    //button functionality
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const buttonText = button.textContent;
+
+            if (buttonText === "CLR"){
+                display.value = "";
+            }
+             else if (buttonText === "DEL"){
+                display.value = display.value.slice(0, -1);
+            }
+            else if (buttonText === "="){
+                try {
+                    display.value = eval(display.value); 
+                }
+                catch (e) {
+                    display.value = "error";
+                }
+            }
+            else {
+                display.value += buttonText;
+            }
+        });
+    });
+ });
+   
+
